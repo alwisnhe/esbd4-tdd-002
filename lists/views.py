869ce4962c08bb23bc48affab58f55f63f9c1f06
Andre Luis Wisnheski <andre.wisnheski@totvs.com.br>
2024-07-19 21:44:46 -0300

    Adicionado except caso vá para uma listagem inexistente. Volta para maxima.

--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,17 @@ def home_page(request):
 	return render(request, 'home.html')
 
 def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	return render(request, 'list.html', {'list': list_})
-
+    try:
+        list_ = List.objects.get(id=list_id)
+    except List.DoesNotExist:
+        max_id = List.objects.latest('id').id
+        list_id = int(list_id) 
+        if list_id > max_id:
+            return redirect(f'/lists/{max_id}/')
+        else:
+            # Se o ID for menor que 1 ou não existir uma lista anterior, redirecione para a primeira lista
+            return redirect('/lists/1/')
+    return render(request, 'list.html', {'list': list_})
 def new_list(request):
 	list_ = List.objects.create()
 	Item.objects.create(text=request.POST['item_text'], list=list_)
